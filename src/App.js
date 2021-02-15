85aaa25cbe5a3c57a7ac00cce447d756ce511166
Angela Diaz <angelamariaconnell@gmail.com>
2021-02-15 14:29:20 -0800

    created recipe and button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { recipes } from './recipes';
+import { useState } from 'react';
+
+function getRecipeByIndex(index){
+  return recipes[index]
+}
 
 function App() {
+ const [currentRecipe, setCurrentRecipe] = useState(0);
+ const nextRecipe = ()=>{
+  const next = currentRecipe +1
+  if(next >= recipes.length){
+    setCurrentRecipe(0)
+  } else {
+    setCurrentRecipe(next)
+  }
+  
+ }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p>{currentRecipe} {getRecipeByIndex(currentRecipe).name}</p>
+       <button onClick={()=> nextRecipe()}>
+         Next Recipe
+       </button>
       </header>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/recipes.js
@@ -0,0 +1,17 @@
+export const recipes = [
+    {
+        name: 'KK\'s Tamale Pie'
+    },
+    {
+        name: 'Anita Menudo'
+    },
+    {
+        name: 'Orange "Chicken"'
+    },
+    {
+        name: 'chi-chi aka Comfort Food'
+    },
+    {
+        name: 'E\'s Burrito'
+    },
+]
